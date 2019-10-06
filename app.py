088669648e91cc7980d91884e8f7dda933570336
le-front <leandro.frontoura@gmail.com>
2019-10-06 22:25:48 +0100

    table can be updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, json, jsonify, make_response
+from flask import Flask, redirect, render_template, request, json 
 from flask_sqlalchemy import SQLAlchemy
 import requests
 
@@ -30,9 +30,10 @@ class Translation_request(db.Model):
     target_language = db.Column(db.String(200))
     text = db.Column(db.Text())
     text_format = db.Column(db.String(200))
+    translatedText = db.Column(db.Text())
     uid = db.Column(db.String(200), unique=True)
 
-    def __init__(self, order_number, price, source_language, status, target_language, text, text_format, uid):
+    def __init__(self, order_number, price, source_language, status, target_language, text, text_format, translatedText, uid):
         self.order_number = order_number
         self.price = price
         self.source_language = source_language
@@ -40,23 +41,23 @@ class Translation_request(db.Model):
         self.target_language = target_language
         self.text = text
         self.text_format = text_format
+        self.translatedText = translatedText 
         self.uid = uid
 
 # variables for the request header
-username = "fullstack-challenge"
-api_key = "9db71b322d43a6ac0f681784ebdcc6409bb83359"
+USERNAME = "fullstack-challenge"
+API_KEY = "9db71b322d43a6ac0f681784ebdcc6409bb83359"
 URL = "https://sandbox.unbabel.com/tapi/v2/translation/"
 
 headers = {
     'Content-Type': 'application/json',
-    'Authorization': f'ApiKey {username}:{api_key}'
+    'Authorization': f'ApiKey {USERNAME}:{API_KEY}'
 }
 
 # app routes
 @app.route('/', methods=["GET", "POST"])
 def index():
     list = Translation_request.query.all()
-    print(list)
     return render_template("index.html", list=list)
 
 
@@ -75,38 +76,65 @@ def translate():
         'target_language': f"{form_target_lang}"
         }
     
-    response = requests.post(URL, data=json.dumps(payload), headers=headers)
-    print(response)
-    json_data = response.json() #json-to-dict sent to DB
+    # post request
+    post_res = requests.post(URL, data=json.dumps(payload), headers=headers)
+    json_data = post_res.json() #json-to-dict sent to DB
+    post_uid = json_data["uid"]
     
+    # get request
+    get_res = requests.get(URL+post_uid, headers=headers)
+    get_data = get_res.json()
 
+    order_number = get_data["order_number"]
+    price = get_data["price"]
+    source_language = get_data["source_language"]
+    status = get_data["status"]
+    target_language = get_data["target_language"]
+    text = get_data["text"]
+    text_format = get_data["text_format"]
+    translatedText = ""
+    uid = get_data["uid"]
+
+    
 # checking if user provided input values properly   
 # TODO check non provided source, target or both languages 
     if form_source_lang == form_target_lang:
         return render_template("warning.html", warning = "Source language and target language must be different.") 
     if form_to_translate == "":
         return render_template("warning.html", warning="Please provide the text for translation")
-
-   
-    order_number = json_data["order_number"]
-    price = json_data["price"]
-    source_language = json_data["source_language"]
-    status = json_data["status"]
-    target_language = json_data["target_language"]
-    text = json_data["text"]
-    text_format = json_data["text_format"]
-    uid = json_data["uid"]
-
-    if response.ok:
-        data = Translation_request(order_number,price, source_language, status, target_language, text, text_format, uid)
+    if post_res.ok:
+        data = Translation_request(order_number, price, source_language, status, target_language, text, text_format, translatedText, uid)
         db.session.add(data)
         db.session.commit()
         
-        # my_query = db.query_all(Translation_request).filter(Translation_request.uid == uid).count()
-        # print(my_query)
+        list = Translation_request.query.all()
+
+        return render_template("success.html", ok_message="Translation successifuly sent", list=list)
+
+@app.route("/update", methods=["GET"])
+def update():
+        
+    list = Translation_request.query.all()
     
 
-        return render_template("success.html", ok_message="Translation successifuly sent")
+    for item in list:
+        
+        get_res = requests.get(URL+item.uid, headers=headers)
+        update = get_res.json()
+
+        if 'translatedText' not in update.keys():
+            item.translatedText = ""
+            item.status = update["status"]
+            db.session.commit()
+        else: 
+            item.translatedText = update["translatedText"]
+            item.status = update["status"]
+            db.session.commit()
+
+
+    list = Translation_request.query.all().order
+    
+    return render_template("index.html", list=list)
 
 
 if __name__ == '__main__':
